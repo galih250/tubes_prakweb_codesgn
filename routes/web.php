c352ba1c0846f094eb0d609844c58841e313d9aa
galih250 <galihk250@gmail.com>
2022-12-21 20:17:25 +0700

    login, register admin dan user

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardUserController;
+use App\Http\Controllers\DashboardAdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +16,19 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+// autentikasi
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
+Route::post('/register', [RegisterController::class, 'store']);
+
+// admin
+Route::get('/dashboard-admin', [DashboardAdminController::class, 'index'])->name('dashboard-admin');
+
+// user
+Route::get('/dashboard-user', [DashboardUserController::class, 'index'])->name('dashboard-user');
 
 Route::get('/', function () {
     return view('welcome');
